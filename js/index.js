93d3c7b4206a792521c5638b0af9b06ac9261cf9
micahjones13 <micahjones13@gmail.com>
2019-05-14 15:17:31 -0600

    finished 6 out of 10 listeners

--- js/index.js
+++ js/index.js
@@ -3,6 +3,11 @@
 //varaible declarations
 const logoHead = document.querySelector('.logo-heading');
 const topImage = document.querySelector('.intro img');
+const heightOutput = document.querySelector('#windowHeight');
+const widthOutput = document.querySelector('#windowWidth');
+const navBar = document.querySelector('.main-navigation');
+const body = document.querySelector('body');
+const para = document.querySelector('.intro p');
 
 //event for mouseover of logoHead
 logoHead.addEventListener('mouseover', (event) => {
@@ -13,7 +18,8 @@ logoHead.addEventListener('mouseover', (event) => {
     }, 500);
 
 })
-//event for resizing of topImage on double click
+
+//event for temp border of topImage on double click
 topImage.addEventListener('dblclick', (event) => {
     topImage.style.border = '2px solid blue';
 
@@ -21,3 +27,54 @@ topImage.addEventListener('dblclick', (event) => {
         event.target.style.border = "";
     }, 1000)
 } )
+
+//function for resizing window and telling the metrics 
+const windowSize = () =>{
+    heightOutput.textContent = window.innerHeight;
+    widthOutput.textContent = window.innerWidth
+}
+window.onresize = windowSize;
+window.addEventListener('resize', windowSize);
+
+//event for nav on scroll
+body.addEventListener('wheel', (event) => {
+    //change the opacity for a limited time
+    navBar.style.opacity = '.5';
+
+    //change it back to solid after a short time
+    setTimeout( function () {
+       navBar.style.opacity = '1';
+    }, 1000)
+})
+
+//scroll
+// const changeBackground = (scrollPos) => {
+
+// }
+let scrollPos = 0;
+let moving = false;
+window.addEventListener('scroll', (event) => {
+    scrollPos = window.scrollY;
+  
+    if (!moving){
+        body.style.backgroundColor = '#F5F5F5';
+        moving = false;
+    }
+    setTimeout(function(){
+        body.style.backgroundColor = 'white';
+    }, 1000)
+
+})
+
+//do something on selection of p content
+para.addEventListener('select', (event) => {
+    para.style.fontSize = '10rem';
+})
+
+//on any key down, content disppears for a bit
+body.addEventListener('keydown', (event) => {
+    body.style.display = 'none';
+    setTimeout(function(){
+        body.style.display = '';
+    }, 1000)
+})
