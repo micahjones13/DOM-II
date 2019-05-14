4e73e5cbc8b64bd5a33f27b8f5e744b0bfd7b578
micahjones13 <micahjones13@gmail.com>
2019-05-14 14:18:20 -0600

    added listeners for logo and top image

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 // Your code goes here
+
+//varaible declarations
+const logoHead = document.querySelector('.logo-heading');
+const topImage = document.querySelector('.intro img');
+
+//event for mouseover of logoHead
+logoHead.addEventListener('mouseover', (event) => {
+    logoHead.style.color = 'purple';
+
+    setTimeout(function(){
+        event.target.style.color = "";
+    }, 500);
+
+})
+//event for resizing of topImage on double click
+topImage.addEventListener('dblclick', (event) => {
+    topImage.style.border = '2px solid blue';
+
+    setTimeout( function(){
+        event.target.style.border = "";
+    }, 1000)
+} )
